3a0698c7abf03c43312bc1b7f42e7e0f90ef22cc
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-06 11:43:11 +0530

    Initial Commit

---
+++ src/com/guessingapp/GuessingApp.java
@@ -0,0 +1,19 @@
+package com.guessingapp;
+
+/**
+ * Guessing App
+ * UC0 - Project Initialization
+ *
+ * Entry point of the application.
+ * This use case sets up the project structure
+ * and verifies that the application runs.
+ *
+ * Author: Developer
+ * Version: 1.0
+ */
+public class GuessingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Guessing App Initialized.");
+    }
+}
